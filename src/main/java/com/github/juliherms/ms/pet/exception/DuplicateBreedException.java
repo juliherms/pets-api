4556416702326c56f69a193d4f646b496d17f07d
Juliherms de Lima Vasconcelos <jlv@cesar-ldap.intranet>
2022-09-14 19:40:19 -0300

    introduce logback configuration

--- src/main/java/com/github/juliherms/ms/pet/exception/DuplicateBreedException.java
+++ src/main/java/com/github/juliherms/ms/pet/exception/DuplicateBreedException.java
@@ -1,8 +1,14 @@
 package com.github.juliherms.ms.pet.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class DuplicateBreedException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
+
+    private static Logger logger = LoggerFactory.getLogger(DuplicateBreedException.class);
+
 
     public DuplicateBreedException(String description) {
         super("Já existe uma raça cadastrada com a descrição = " + description);
--- src/main/java/com/github/juliherms/ms/pet/exception/NotFoundException.java
+++ src/main/java/com/github/juliherms/ms/pet/exception/NotFoundException.java
@@ -1,10 +1,16 @@
 package com.github.juliherms.ms.pet.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class NotFoundException  extends RuntimeException {
+
+    private static Logger logger = LoggerFactory.getLogger(NotFoundException.class);
 
     private static final long serialVersionUID = 1L;
 
     public NotFoundException(String message) {
         super(message);
+        logger.warn(message);
     }
 }
